e6e5a360494d2c690021e4b6a15baa5611073ece
fengfeng <603252728@qq.com>
2023-12-23 20:16:07 +0800

    change the style

--- src/views/scheduledTask/checkPlan/index.vue
+++ src/views/scheduledTask/checkPlan/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="BL-common-layout">
-    <div class="BL-common-layout-center">
+    <div class="BL-common-layout-center BL-flex-main">
       <el-row class="BL-common-search-box" :gutter="16">
         <el-form @submit.native.prevent>
           <el-col :span="6">
